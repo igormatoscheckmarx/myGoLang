d193ab75e958c1848743f8f93fe20f3eb1e28981
Igor Matos <94920985+igormatoscheckmarx@users.noreply.github.com>
2022-03-18 16:29:06 +0000

    Update basic.go

--- basic.go
+++ basic.go
@@ -1,1 +1,24 @@
 
+//basic Micro service
+// go run basic.go
+//new terminal -- curl -v -d 'World' localhost:9090
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	//fmt.Println("Hello, World!")
+	http.HandleFunc("/", func(rw http.ResponseWriter, rs *http.Request) {
+		d, _ := ioutil.ReadAll(rs.Body)
+		fmt.Fprintf(rw, "Hello %s", d)
+	})
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("Goodbye World")
+	})
+	http.ListenAndServe(":9090", nil)
+}
